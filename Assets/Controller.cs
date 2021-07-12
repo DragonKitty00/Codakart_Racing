534fe04f99cbd7d01935e0dfc306f50d5929112f
DragonKitty00 <87153689+DragonKitty00@users.noreply.github.com>
2021-07-12 16:14:35 -0400

    added some commets
    Just some added commets

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -12,21 +12,28 @@ public class Axle
     public bool steering;
 }
 
+//Controller class that moves the kart wheels
 public class Controller : MonoBehaviour
 {
+    //list of the axles and motor/steering for the kart wheels
     public List<Axle> axles;
     public float maxMoterTorque;
     public float maxSteeringAngle;
-
+    
+    //fixed update that runs the pysics and input
     public void FixedUpdate()
     {
-        float motor = maxMoterTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        //get the motor and steering input WASD
+        float motor = maxMoterTorque * Input.GetAxis("Vertical"); //W-Forwards, S-Backwards
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");//A-Left, D-right
 
+        //foreach loop that runs through all the axles ing the list
         foreach (Axle axle in axles)
         {
+            //if motor is valid, move the wheels
             if (axle.motor)
             {
+                //if steering is valid, turn the wheels
                 axle.leftWheel.motorTorque = motor;
                 axle.rightWheel.motorTorque = motor;
             }
